f5200b0d78b4233d043d912529f3e2583848cb13
Anwesh43 <anweshthecool0@gmail.com>
2020-04-27 05:55:39 +0530

    adding BLSNode class

--- ballsteplineview/src/main/java/com/anwesh/uiprojects/ballsteplineview/BallStepLineView.kt
+++ ballsteplineview/src/main/java/com/anwesh/uiprojects/ballsteplineview/BallStepLineView.kt
@@ -113,4 +113,45 @@ class BallStepLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLSNode(var i : Int, val state : State = State()) {
+
+        private var next : BLSNode? = null
+        private var prev : BLSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BLSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLSNode(i, state.scale, paint)
+        }
+
+        fun update(cb :(Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLSNode {
+            var curr : BLSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
