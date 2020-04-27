a3290d91f2aa17c5c37e18fad3856deba7343081
Anwesh43 <anweshthecool0@gmail.com>
2020-04-27 06:32:46 +0530

    using renderer in view

--- ballsteplineview/src/main/java/com/anwesh/uiprojects/ballsteplineview/BallStepLineView.kt
+++ ballsteplineview/src/main/java/com/anwesh/uiprojects/ballsteplineview/BallStepLineView.kt
@@ -53,14 +53,16 @@ fun Canvas.drawBLSNode(i : Int, scale : Float, paint : Paint) {
 
 class BallStepLineView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -182,8 +184,9 @@ class BallStepLineView(ctx : Context) : View(ctx) {
 
         private val animator : Animator = Animator(view)
         private val bsl : BallStepLine = BallStepLine(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-        fun render(canvas : Canvas, paint : Paint) {
+        fun render(canvas : Canvas) {
             canvas.drawColor(backColor)
             bsl.draw(canvas, paint)
             animator.animate {
