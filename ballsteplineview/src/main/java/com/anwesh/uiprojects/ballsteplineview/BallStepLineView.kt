4a0c4120542e4ee086a073953440fccc28abc09f
Anwesh43 <anweshthecool0@gmail.com>
2020-04-27 07:17:00 +0530

    adding fixes

--- ballsteplineview/src/main/java/com/anwesh/uiprojects/ballsteplineview/BallStepLineView.kt
+++ ballsteplineview/src/main/java/com/anwesh/uiprojects/ballsteplineview/BallStepLineView.kt
@@ -16,7 +16,7 @@ val nodes : Int = 5
 val parts : Int = 2
 val scGap : Float = 0.02f / parts
 val strokeFactor : Float = 90f
-val sizeFactor : Int = 90
+val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
@@ -25,16 +25,16 @@ val rFactor : Float = 3f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
-fun Float.sinify() : Float = 1f / this
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
 
-fun Canvas.drawBallLineStep(size : Float, scale : Float, paint : Paint) {
+fun Canvas.drawBallLineStep(size : Float, gap : Float, scale : Float, paint : Paint) {
     val r : Float = size / rFactor
     val sc1 : Float = scale.divideScale(0, parts)
     val sc2 : Float = scale.divideScale(1, parts)
     val sf : Float = sc1.sinify()
-    drawCircle(size * sf, 0f, r * (1 - sc2), paint)
-    drawLine(0f, 0f, size * sf, 0f, paint)
-    drawCircle(size, 0f, r * sc2, paint)
+    drawCircle(gap * sf, 0f, r * (1 - sc2), paint)
+    drawLine(0f, 0f, gap * sf, 0f, paint)
+    drawCircle(gap, 0f, r * sc2, paint)
 }
 
 fun Canvas.drawBLSNode(i : Int, scale : Float, paint : Paint) {
@@ -47,7 +47,7 @@ fun Canvas.drawBLSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     save()
     translate(gap * (i + 1), h / 2)
-    drawBallLineStep(size, scale, paint)
+    drawBallLineStep(size, gap, scale, paint)
     restore()
 }
 
